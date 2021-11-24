87b698bfd028617f7b29ce59347cd215faffda7e
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-11-24 10:47:26 +0100

    Improved test

--- tests/test_acquire.py
+++ tests/test_acquire.py
@@ -12,13 +12,14 @@ instrument.add_control('P2', 0, [3])
 instrument.add_readout('R1', 1, [0,1])
 
 p = instrument.new_program('acquire')
-p.repetitions = 1000
+p.repetitions = 100
 
 P1 = p.P1
 P2 = p.P2
 R1 = p.R1
 
-R1.add_acquisition_bins('default', 10)
+R1.add_acquisition_bins('non-weighed', 100)
+R1.add_acquisition_bins('weighed', 100)
 R1.add_weight('gaus100', signal.gaussian(100, 12))
 
 amplitude = 0.125
@@ -26,12 +27,12 @@ amplitude = 0.125
 with p.parallel():
     P1.block_pulse(100, 0.25)
     P2.block_pulse(100, -0.25)
-    R1.acquire('default', 'increment')
+    R1.acquire('non-weighed', 'increment')
 
 with p.parallel():
     P1.block_pulse(100, 0.25)
     P2.block_pulse(100, -0.25)
-    R1.acquire_weighed('default', 'increment', 'gaus100')
+    R1.acquire_weighed('weighed', 'increment', 'gaus100')
 
 p.describe()
 print()
