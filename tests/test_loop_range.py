5222f9da415e054fdc995e5d70b2e5375f290834
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2021-11-24 14:06:17 +0100

    Test loop_range

---
+++ tests/test_loop_range.py
@@ -0,0 +1,39 @@
+
+from q1pulse.instrument import Q1Instrument
+
+from init_pulsars import qcm0
+
+instrument = Q1Instrument()
+instrument.add_qcm(0, qcm0)
+instrument.add_control('P1', 0, [2])
+instrument.add_control('P2', 0, [3])
+
+p = instrument.new_program('loop_range')
+p.repetitions = 3
+
+P1 = p.P1
+P2 = p.P2
+
+with p.loop_range(20, 100, 20) as t_wait:
+    P1.block_pulse(20, -0.1)
+    P1.block_pulse(20, 0.8)
+    p.wait(t_wait)
+    P2.block_pulse(20, 0.6)
+    p.wait(100)
+
+# 4 pulses
+with p.loop_range(-2, 2, 1) as t_wait:
+    P1.block_pulse(20, 0.2)
+    p.wait(20)
+
+# 4 pulses
+with p.loop_range(80, 0, -20) as t_wait:
+    P1.block_pulse(20, 0.1)
+    p.wait(t_wait)
+
+#p.describe()
+#print()
+
+p.compile(verbose=True, listing=True, annotate=True)
+
+instrument.run_program(p)
