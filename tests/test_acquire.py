0e486ff51ef8238ded824b30c082e669a2f8fdc0
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2021-11-24 14:06:52 +0100

    Tested acquire and acquire_weighed

--- tests/test_acquire.py
+++ tests/test_acquire.py
@@ -1,3 +1,4 @@
+import numpy as np
 import scipy.signal as signal
 
 from q1pulse.instrument import Q1Instrument
@@ -7,36 +8,73 @@ from init_pulsars import qcm0, qrm1
 instrument = Q1Instrument()
 instrument.add_qcm(0, qcm0)
 instrument.add_qrm(1, qrm1)
-instrument.add_control('P1', 0, [2])
-instrument.add_control('P2', 0, [3])
-instrument.add_readout('R1', 1, [0,1])
+instrument.add_control('P1', 0, [0])
+instrument.add_control('P2', 1, [1])
+instrument.add_readout('R1', 1, [])
 
 p = instrument.new_program('acquire')
-p.repetitions = 100
+p.repetitions = 1
 
 P1 = p.P1
 P2 = p.P2
 R1 = p.R1
 
-R1.add_acquisition_bins('non-weighed', 100)
-R1.add_acquisition_bins('weighed', 100)
+N = 7
+n_acq = N*N*p.repetitions
+R1.add_acquisition_bins('non-weighed', n_acq)
+R1.add_acquisition_bins('weighed', n_acq)
 R1.add_weight('gaus100', signal.gaussian(100, 12))
 
 amplitude = 0.125
 
-with p.parallel():
-    P1.block_pulse(100, 0.25)
-    P2.block_pulse(100, -0.25)
-    R1.acquire('non-weighed', 'increment')
+# output range QCM (P1): +/- 2.5 V
+# output range QRM (P1): +/- 0.5 V
+# input range QRM (R1): +/- 0.5 V
 
-with p.parallel():
-    P1.block_pulse(100, 0.25)
-    P2.block_pulse(100, -0.25)
-    R1.acquire_weighed('weighed', 'increment', 'gaus100')
+v1_max = 0.5/2.5*0.9
+v2_max = 0.5/0.5*0.9
 
-p.describe()
-print()
+with p.loop_linspace(-v1_max, v1_max, N) as v1:
+    with p.loop_linspace(-v2_max, v2_max, N) as v2:
+        with p.parallel():
+            P1.block_pulse(500, v1)
+            P2.block_pulse(500, v2)
+            # delay from output to input is ~108 ns
+            R1.acquire('non-weighed', 'increment', t_offset=112)
 
-p.compile(verbose=True, listing=True, annotate=True)
+        with p.parallel():
+            P1.block_pulse(500, v1)
+            P2.block_pulse(500, v2)
+            R1.acquire_weighed('weighed', 'increment', 'gaus100', t_offset=120)
+
+        p.wait(1100)
+
+#p.describe()
+#print()
+
+p.compile(listing=True, annotate=True)
+
+# @@@ add to program.readout
+qrm1.sequencer0_integration_length_acq(100)
+qrm1.sequencer1_integration_length_acq(100)
+qrm1.in0_gain(0)
+qrm1.in1_gain(0)
 
 instrument.run_program(p)
+
+# @@@ program should return data.
+data_n = instrument.get_acquisition_bins('R1', 'non-weighed')
+data_w = instrument.get_acquisition_bins('R1', 'weighed')
+
+dn0 = np.array(data_n['integration']['path0']).reshape((p.repetitions,N,N))
+dn1 = np.array(data_n['integration']['path1']).reshape((p.repetitions,N,N))
+dw0 = np.array(data_w['integration']['path0']).reshape((p.repetitions,N,N))
+dw1 = np.array(data_w['integration']['path1']).reshape((p.repetitions,N,N))
+
+with np.printoptions(precision=2, threshold=1000):
+    print('non-weighed')
+    print(dn0)
+    print(dn1)
+    print('weighed')
+    print(dw0)
+    print(dw1)
