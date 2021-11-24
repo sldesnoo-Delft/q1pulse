d3537694541369c22eee4d66f01a1086f2cc8331
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2021-11-24 14:24:19 +0100

    Improved test code

--- tests/test_sync_wave.py
+++ tests/test_sync_wave.py
@@ -1,5 +1,4 @@
 import numpy as np
-from pprint import pprint
 from q1pulse.instrument import Q1Instrument
 
 from init_pulsars import qcm0, qrm1
@@ -13,19 +12,25 @@ instrument.add_control('P2', 0, [1])
 p = instrument.new_program('sync_wave')
 p.repetitions = 10_000_000
 
+N = 10
 P1 = p.P1
 P2 = p.P2
-block = np.concatenate([
-        -0.5*np.ones(10),
-        -0.5*np.ones(10),
-        ])
-#block = np.concatenate([
-#        np.linspace(0,-0.5, 2, endpoint=False),
-#        -0.5*np.ones(6),
-#        np.linspace(-0.5, 0.5, 4, endpoint=False),
-#        0.5*np.ones(6),
-#        np.linspace(0.5, 0, 2, endpoint=False),
-#        ])
+
+ramped = True
+
+if ramped:
+    block = np.concatenate([
+            np.linspace(0,-0.5, 2, endpoint=False),
+            -0.5*np.ones(N-4),
+            np.linspace(-0.5, 0.5, 4, endpoint=False),
+            0.5*np.ones(N-4),
+            np.linspace(0.5, 0, 2, endpoint=False),
+            ])
+else:
+    block = np.concatenate([
+            -0.5*np.ones(N),
+            0.5*np.ones(N),
+            ])
 P1.add_wave('block', block)
 P2.add_wave('block', block)
 
@@ -36,7 +41,7 @@ with p.loop_range(2):
     with p.parallel():
         P1.play('block')
         P2.play('block')
-        p.wait(20)
+        p.wait(2*N)
 
 p.wait(10000)
 p.describe()
