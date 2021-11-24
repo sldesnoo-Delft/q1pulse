18900a3e5b907c04acebcd270d7ea8c9295774b2
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2021-11-24 15:56:00 +0100

    Charge stability diagram tests

--- tests/test_csd_ramp.py
+++ tests/test_csd_ramp.py
@@ -10,29 +10,28 @@ instrument.add_control('P1', 0, [2])
 instrument.add_control('P2', 0, [3])
 instrument.add_readout('R1', 1, [1])
 
-p = instrument.new_program('csd')
+p = instrument.new_program('csd_ramp')
 P1 = p.P1
 P2 = p.P2
 R1 = p['R1'] # using alternative notation
 
-N = 10
+N = 100
 R1.add_acquisition_bins('default', N*N)
 
 gates=[P1, P2] # alternative notation: ['P1', 'P2']
 v_init = [120, 40]
 v_manip = [0, 0]
 v_read = [-30, 60]
-t_measure = 2000
+t_measure = 2_000
 t_acqdelay = 500
 t_step = t_measure + t_acqdelay
 
 with p.loop_linspace(-0.5, 0.5, N) as v2:
-    with p.loop_linspace(-0.5, 0.5, N) as v1:
-        with p.parallel():
-            p.wait(t_step)
-            P2.set_offset(v2)
-            P1.set_offset(v1)
-            R1.acquire('default', 'increment', t_offset=t_acqdelay)
+    with p.parallel():
+        p.wait(t_step*N+4)
+        P2.set_offset(v2)
+        P1.ramp(t_step*N, -0.5, 0.5)
+        R1.acquire('default', 'increment', t_offset=t_acqdelay)
 
 P1.set_offset(0.0)
 P2.set_offset(0.0)
