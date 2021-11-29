7a346fa23eee5288ccd597631f7e0e4846ea710e
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-11-29 14:59:48 +0100

    Added all generator methods

--- q1pulse/lang/generator.py
+++ q1pulse/lang/generator.py
@@ -1,8 +1,38 @@
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 
 class GeneratorBase(ABC):
+    @property
+    @abstractmethod
+    def repetitions(self):
+        pass
+
+    @repetitions.setter
+    @abstractmethod
+    def repetitions(self, value):
+        pass
+
+    @abstractmethod
+    def start_main(self):
+        pass
+
+    @abstractmethod
+    def finalize(self):
+        pass
+
     @abstractmethod
     def add_comment(self, line, init_section=False):
+        pass
+
+    @abstractmethod
+    def set_label(self, label):
+        '''
+        Label to be added to next instruction
+        '''
+        pass
+
+    @abstractmethod
+    def adjust_time(self, duration):
         pass
 
     @abstractmethod
@@ -66,6 +96,10 @@ class GeneratorBase(ABC):
         pass
 
     @abstractmethod
+    def set_mrk(self, time, value):
+        pass
+
+    @abstractmethod
     def awg_offset(self, time, offset0, offset1):
         pass
 
@@ -74,9 +108,39 @@ class GeneratorBase(ABC):
         pass
 
     @abstractmethod
+    def reset_phase(self, time):
+        pass
+
+    @abstractmethod
+    def set_phase(self, time, phase, hires_regs):
+        pass
+
+    @abstractmethod
+    def add_phase(self, time, delta, hires_regs):
+        pass
+
+    @abstractmethod
     def play(self, time, wave0, wave1):
         pass
 
     @abstractmethod
     def acquire(self, time, section, bin_index):
+        pass
+
+    @abstractmethod
+    def acquire_weighed(self, time, bins, bin_index, weight0, weight1):
+        pass
+
+    @abstractmethod
+    @contextmanager
+    def scope(self):
+        pass
+
+    @abstractmethod
+    def get_temp_reg(self):
+        ''' Allocates an assembler register for temporary use within scope. '''
+        pass
+
+    @abstractmethod
+    def allocate_reg(self, name):
         pass
