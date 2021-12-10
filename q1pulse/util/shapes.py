7819ee13ccbd5781c90ef6225f9ebf834bcbd6c8
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-12-10 17:18:51 +0100

    Fixed amplitude of Q channel

--- q1pulse/util/shapes.py
+++ q1pulse/util/shapes.py
@@ -16,4 +16,4 @@ def add_chirp(duration, f_start, f_stop, amplitude,
         phase = 2*np.pi * (f_step * 0.5 * t**2 + f_start*t)
         wave0 = seq.add_wave(f'chirp_I{i}', np.sin(phase))
         wave1 = seq.add_wave(f'chirp_Q{i}', np.cos(phase))
-        seq.shaped_pulse(wave0, amplitude, wave1)
+        seq.shaped_pulse(wave0, amplitude, wave1, amplitude)
