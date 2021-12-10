c6258691aee9efd4669460eb0a854c82d44929ab
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2021-12-10 17:22:35 +0100

    Added plot function when Q1Simulator is used.

---
+++ tests/plot_util.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as pt
+
+def plot_output(instruments):
+    for instrument in instruments:
+        if hasattr(instrument, 'plot'):
+            pt.figure()
+            pt.title(instrument.name)
+            instrument.plot()
+            pt.legend()
