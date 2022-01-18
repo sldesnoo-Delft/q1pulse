16a338bdd8a375a32d42db1d88e33fd081bbe0cf
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-01-18 14:28:51 +0100

    improved test

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -10,22 +10,26 @@ instrument.add_control('P1', qrm1.name, [0])
 instrument.add_control('P2', qcm0.name, [1])
 instrument.add_control('P3', qcm0.name, [2])
 instrument.add_control('P4', qcm0.name, [3])
+instrument.add_control('R1', qrm1.name, [1])
 
 p = instrument.new_program('sync')
 p.repetitions = 10_000_000
 
+t = 5000
+a = 1.0
+
 P1 = p.P1
 P2 = p.P2
 
-gates=['P1', 'P2', 'P3', 'P4']
-with p.loop_range(2):
-    p.set_offsets(gates, [-0.1, -0.1, -0.1, -0.1])
-    p.wait(12)
-    p.set_offsets(gates, [0.1, 0.1, 0.1, 0.1])
-    p.wait(12)
+gates=['P1', 'P2', 'P3', 'P4', 'R1']
+with p.loop_range(1000):
+    p.set_offsets(gates, [-a]*5)
+    p.wait(t)
+    p.set_offsets(gates, [a]*5)
+    p.wait(t)
 
-p.set_offsets(gates, [0.0, 0.0, 0.0, 0.0])
-p.wait(10000)
+#p.set_offsets(gates, [0.0]*4)
+#p.wait(t)
 p.describe()
 
 p.compile(listing=True)
