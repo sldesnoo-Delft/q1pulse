e941016aa4bbafc6389474f1dd847f862bd89e92
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-12-02 15:38:43 +0100

    Bump version: 0.6.0 → 0.7.0

--- q1pulse/__init__.py
+++ q1pulse/__init__.py
@@ -1,4 +1,4 @@
 from .instrument import Q1Instrument
 from .lang.math_expressions import Lsr as lsr
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1pulse']
 print('packages: {}'.format(packages))
 
 setup(name="q1pulse",
-	version="0.6.0",
+	version="0.7.0",
     author="Sander de Snoo",
 	packages = find_packages(),
     python_requires=">=3.7",
