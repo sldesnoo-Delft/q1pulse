18cfa454fe8ff68864fd75cafaf8001d77521722
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-12-21 17:50:16 +0100

    test chirp with range excluding 0.0 Hz

--- tests/test_chirp.py
+++ tests/test_chirp.py
@@ -1,19 +1,21 @@
 
-from q1pulse.instrument import Q1Instrument
 
 from init_pulsars import qcm0
 from plot_util import plot_output
 
+#%%
+from q1pulse.instrument import Q1Instrument
 instrument = Q1Instrument('q1')
 instrument.add_qcm(qcm0)
 instrument.add_control('q1', qcm0.name, [0,1], nco_frequency=0)
 
+#%%
 p = instrument.new_program('chirp')
 p.repetitions = 1
 
 q1 = p.q1
 
-q1.chirp(20_000, 0.5, -10e6, 10e6)
+q1.chirp(20_000, 0.5, 1e6, 10e6)
 
 p.describe()
 
