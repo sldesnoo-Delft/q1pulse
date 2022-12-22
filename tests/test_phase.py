c3a18f38c27ca5f138db238933d074e0b5119bc2
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-12-22 09:43:48 +0100

    updated number of q1core cycles for set_phase and shift_phase with register

--- tests/test_phase.py
+++ tests/test_phase.py
@@ -8,21 +8,23 @@ instrument.add_qcm(qcm0)
 instrument.add_control('P1', qcm0.name, [2])
 
 p = instrument.new_program('phase')
-p.repetitions = 10
+p.repetitions = 100
 
 P1 = p.P1
 
 p.R.phase1 = 0.125
 P1.Rs.phase2 = -0.125
-
+p.wait(60)
 P1.set_phase(0.5)
-p.wait(100)
+
+p.wait(348)
 P1.set_phase(p.R.phase1)
-p.wait(300)
-P1.shift_phase(P1.Rs.phase2)
-p.wait(300)
+
+p.wait(188)
+P1.shift_phase(P1.Rs.phase2,hires_reg=False)
+
+p.wait(348)
 P1.shift_phase(P1.Rs.phase2 + 0.1)
-p.wait(300)
 
 p.describe()
 
