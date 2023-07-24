6623267963a3119d6dfcf2fad3b0c47cff0a8940
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-07-24 14:52:00 +0200

    Bump to 0.9.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1pulse']
 print('packages: {}'.format(packages))
 
 setup(name="q1pulse",
-	version="0.8.8",
+	version="0.9.0",
     author="Sander de Snoo",
 	packages = find_packages(),
     python_requires=">=3.7",
