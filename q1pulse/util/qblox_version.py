322d13f5124b4891c234e5e458a1a57d00cee91d
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-12-07 15:00:22 +0100

    accept qblox-instruments up to 0.11.2

--- q1pulse/util/qblox_version.py
+++ q1pulse/util/qblox_version.py
@@ -4,7 +4,7 @@ from packaging.version import Version
 from q1pulse import __version__ as q1pulse_version
 
 def check_qblox_instrument_version():
-    if qblox_version > Version('0.11.1'):
+    if qblox_version > Version('0.11.2'):
         print(f'WARNING Q1Pulse {q1pulse_version} has not been tested on qblox_instruments version {qblox_version}')
     elif qblox_version < Version('0.9'):
         raise Exception('Q1Pulse {q1pulse_version} requires qblox_instruments version  v0.9+')
