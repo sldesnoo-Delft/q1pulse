e26449f3c1e126c1932714913b394b2502b0a9d4
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2024-03-18 17:52:42 +0100

    Bump to 0.11.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['q1pulse']
 print('packages: {}'.format(packages))
 
 setup(name="q1pulse",
-	version="0.11.6",
+	version="0.11.7",
     author="Sander de Snoo",
 	packages = find_packages(),
     python_requires=">=3.7",
