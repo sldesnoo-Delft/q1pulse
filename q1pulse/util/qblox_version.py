9adf524b95cc0426f9927e648c27bdef06be7c1e
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-01-17 08:56:39 +0100

    Update to version 0.15.0

--- q1pulse/util/qblox_version.py
+++ q1pulse/util/qblox_version.py
@@ -3,12 +3,14 @@ from qblox_instruments import __version__ as qblox_version_str
 from packaging.version import Version
 from q1pulse import __version__ as q1pulse_version
 
+
 def check_qblox_instrument_version():
     min_version = "0.11.0"
-    max_version = "0.14.1"
+    max_version = "0.15.0"
     if qblox_version < Version(min_version):
         raise Exception(f'Q1Pulse {q1pulse_version} requires qblox_instruments {min_version}+')
     if qblox_version > Version(max_version):
         print(f'WARNING Q1Pulse {q1pulse_version} has not been tested on qblox_instruments {qblox_version}')
+
 
 qblox_version = Version(qblox_version_str)
