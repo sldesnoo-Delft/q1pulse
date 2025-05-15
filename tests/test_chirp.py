073c7af1062106151a44242bd6f6fbba16789329
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-05-15 08:48:12 +0200

    style cleanup

--- tests/test_chirp.py
+++ tests/test_chirp.py
@@ -3,13 +3,13 @@
 from init_pulsars import qcm0
 from plot_util import plot_output
 
-#%%
+# %%
 from q1pulse.instrument import Q1Instrument
 instrument = Q1Instrument('q1')
 instrument.add_qcm(qcm0)
-instrument.add_control('q1', qcm0.name, [0,1], nco_frequency=0)
+instrument.add_control('q1', qcm0.name, [0, 1], nco_frequency=0)
 
-#%%
+# %%
 p = instrument.new_program('chirp')
 p.repetitions = 1
 
