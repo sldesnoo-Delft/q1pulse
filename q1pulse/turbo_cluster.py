f5530d2ad92fa7a51082da299899f1f8474fc42a
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-06-02 09:01:16 +0200

    Added API documentation

--- q1pulse/turbo_cluster.py
+++ q1pulse/turbo_cluster.py
@@ -26,7 +26,29 @@ logger = logging.getLogger(__name__)
 
 
 class TurboCluster(Cluster):
+    """
+    `TurboCluster` is modified version of `qblox_instruments.Cluster` that speeds up communication
+    with a factor 2 to 10.
+
+    TurboCluster is compatible with qblox_instruments v0.11 to v0.17 and can be used as a drop-in
+    replacement for Cluster when Q1Pulse is used.
+
+    Other control code only needs some minor modifications.
+    TurboCluster a TCP connection per module and therefore `get_num_system_error(slot)` should be called for
+    every module. Or better, `get_system_errors()` should be called to collect the errors for
+    all connections with a concurrent operation.
+    The status of many sequencers can be requested with the fast concurrent call `get_sequencer_status_multiple`.
+
+    The speedup is realized by:
+        * Using a TCP/IP connection per module allowing commands to be sent to multiple modules concurrently.
+        * Sending batches of commands and requests to a module and then read the batch of responses.
+        * Caching module and sequencer configuration.
+    """
+
     use_configuration_cache = True
+    """If true use configuration cache. This makes configuration changes must
+    faster, but could fail in scenarios where other Cluster objects can modify the configuration.
+    """
 
     def __init__(
             self,
@@ -45,29 +67,12 @@ class TurboCluster(Cluster):
         self._ip_address = addr_info.address
         self._connections: dict[int, Ieee488_2] = {}
         self._needs_check: dict[int, bool] = {}
-        self._init_cache()
         for slot in range(1, 21):
             ip_config = resolve(f"{self._ip_address}/{slot}")
             transport = IpTransport(ip_config.address, ip_config.scpi_port, timeout=5.0)
             self._connections[slot] = Ieee488_2(transport)
         super().__init__(name, identifier, port, debug=2)
-        if TurboCluster.use_configuration_cache:
-            attr_names = [
-                "_get_sequencer_config",
-                "_set_sequencer_config",
-                "_get_sequencer_channel_map",
-                "_set_sequencer_channel_map",
-                "_get_pre_distortion_config",
-                "_set_pre_distortion_config",
-                ]
-            mod_handles = self._mod_handles
-            for slot_id in range(1, 21):
-                if slot_id in mod_handles and "func_refs" in mod_handles[slot_id]:
-                    func_refs = self._mod_handles[slot_id]["func_refs"]
-                    for name in attr_names:
-                        if name in func_refs._funcs:
-                            func_refs.register(partial(getattr(self, name), slot_id), name)
-                            # logger.debug(f"Registered {slot_id}: {name}")
+        self._init_configuration_cache()
 
     def _write(self, cmd_str):
         conn, cmd = self._get_connection_and_remove_slot(cmd_str)
@@ -102,6 +107,13 @@ class TurboCluster(Cluster):
                 self._needs_check[slot] = True
                 return self._connections[slot], module_cmd
         return super(), cmd
+
+    # ------------------------------------------------------------------
+    # Versions <= v0.16 call arm_sequencer start_sequencer and stop_sequencer directly on the original
+    # `_write ` method of ClusterScpi. Thus, all calls will go to CMM. See `ClusterNative._create_module_funcrefs`.
+    # It's better and safer to have all calls to a module go directly to the module
+    # using the same socket and not some calls via another socket.
+    # ------------------------------------------------------------------
 
     def arm_sequencer(self, slot: int | None = None, sequencer: int | None = None) -> None:
         """
@@ -166,6 +178,38 @@ class TurboCluster(Cluster):
             sequencer = ""  # Arm all sequencers within a module
 
         return self._write(f"SLOT{slot}:SEQuencer{sequencer}:START")
+
+    def stop_sequencer(self, slot: int | None = None, sequencer: int | None = None) -> None:
+        """
+        Stop the indexed sequencer, thereby putting it in the stopped state. If
+        an invalid sequencer index is given, an error is set in system error. If
+        no sequencer index is given, all sequencers are stopped.
+
+        Parameters
+        ----------
+        slot : Optional[int]
+            The slot index of the module being referred to.
+        sequencer : Optional[int]
+            Sequencer index.
+
+        Raises
+        ----------
+        RuntimeError
+            An error is reported in system error and debug <= 1.
+            All errors are read from system error and listed in the exception.
+        """
+        if slot is None:
+            slot = ""  # Stop sequencers across all modules
+
+        if sequencer is None:
+            sequencer = ""  # Stop all sequencers within a module
+
+        self._write(f"SLOT{slot}:SEQuencer{sequencer}:STOP")
+
+    # ----------------------------------------------------------------
+    # System error is a state per connection. Therefore TurboCluster has added the optional
+    # slot argument to get the error count and message per connection.
+    # ----------------------------------------------------------------
 
     def get_system_error(self, slot: int | None = None) -> str:
         """
@@ -245,6 +289,14 @@ class TurboCluster(Cluster):
 
         return errors
 
+    # ----------------------------------------------------------------
+    # Polling the sequencer status for every sequencer in a sequential way takes a lot of time.
+    # The method below sends all the status requests for all the sequencers and
+    # then reads all the responses.
+    # Note that the implementation uses a new implementation of `readline` that can
+    # read a sequence of response in a safe way without losing data.
+    # ----------------------------------------------------------------
+
     def get_sequencer_status_multiple(self, sequencers: dict[int, list[int]]) -> tuple[int, int, object]:
         """
         Returns the status for multiple sequencers using parallel requests.
@@ -276,11 +328,44 @@ class TurboCluster(Cluster):
                 results.append((slot, sequencer, status))
         return results
 
+    # --------------------------------------------------------------------------------
+    # The following methods are added to cache the sequencer configuration and
+    # reduce the amount of configuration requests every time a setting is changed.
+    #
+    # The methods explictly call ClusterScpi, because in versions <= v0.16 the
+    # super class (ClusterNative) calls the methods via func_refs.
+    # Calls are mode to `ClusterScpi.method_xxx(self, ...)`, because method invocation on
+    # super() will result in infinite recursion (for versions <= v0.16).
+    # --------------------------------------------------------------------------------
+
+    def _init_configuration_cache(self):
+        if TurboCluster.use_configuration_cache:
+            # NOTE: A simple override of the methods does not work for versions <= v0.16.
+            #       These versions use Funcs to call the operations on ClusterScpi from
+            #       multiple methods in ClusterNative.
+            attr_names = [
+                "_get_sequencer_config",
+                "_set_sequencer_config",
+                "_get_sequencer_channel_map",
+                "_set_sequencer_channel_map",
+                "_get_pre_distortion_config",
+                "_set_pre_distortion_config",
+                ]
+            mod_handles = self._mod_handles
+            for slot_id in range(1, 21):
+                if slot_id in mod_handles and "func_refs" in mod_handles[slot_id]:
+                    func_refs = self._mod_handles[slot_id]["func_refs"]
+                    for name in attr_names:
+                        if name in func_refs._funcs:
+                            func_refs.register(partial(getattr(self, name), slot_id), name)
+                            # logger.debug(f"Registered {slot_id}: {name}")
+        self._clear_cache()
+
     def reset(self):
-        self._init_cache()
+        self._clear_cache()
         super().reset()
 
-    def _init_cache(self):
+    def _clear_cache(self):
         self._channel_map_cache: dict[tuple[int, int], str] = {}
         self._sequencer_config_cache: dict[tuple[int, int], str] = {}
         self._slot_predistortion_cache: dict[int, str] = {}
@@ -289,7 +374,10 @@ class TurboCluster(Cluster):
         self, slot: int, sequencer: int, sequencer_channel_map: Any
     ) -> None:
         """
-        Set channel map of the indexed sequencer. The channel map consists of a list with two elements representing the components of the complex signal to be acquired, each mapping to a list of DAC indices. Index i maps to output i+1. If an invalid sequencer index is given or the channel map is not valid, an error is set in system error.
+        Set channel map of the indexed sequencer. The channel map consists of a list with two elements representing
+        the components of the complex signal to be acquired, each mapping to a list of DAC indices.
+        Index i maps to output i+1. If an invalid sequencer index is given or the channel map is not valid,
+        an error is set in system error.
 
         Parameters
         ----------
@@ -318,7 +406,10 @@ class TurboCluster(Cluster):
 
     def _get_sequencer_channel_map(self, slot: int, sequencer: int) -> Any:
         """
-        Get channel map of the indexed sequencer. The channel map consists of a list with two elements representing the components of the complex signal to be acquired, each mapping to a list of DAC indices. Index i maps to output i+1. If an invalid sequencer index is given or the channel map is not valid, an error is set in system error.
+        Get channel map of the indexed sequencer. The channel map consists of a list with two elements representing
+        the components of the complex signal to be acquired, each mapping to a list of DAC indices.
+        Index i maps to output i+1. If an invalid sequencer index is given or the channel map is not valid,
+        an error is set in system error.
 
         Parameters
         ----------
@@ -357,7 +448,8 @@ class TurboCluster(Cluster):
         self, slot: int, sequencer: int, sequencer_config: Any
     ) -> None:
         """
-        Set configuration of the indexed sequencer. The configuration consists of multiple parameters in a JSON format. If an invalid sequencer index is given, an error is set in system error.
+        Set configuration of the indexed sequencer. The configuration consists of multiple parameters in a JSON format.
+        If an invalid sequencer index is given, an error is set in system error.
 
         Parameters
         ----------
@@ -387,7 +479,8 @@ class TurboCluster(Cluster):
 
     def _get_sequencer_config(self, slot: int, sequencer: int) -> Any:
         """
-        Get configuration of the indexed sequencer. The configuration consists of multiple parameters in a JSON format. If an invalid sequencer index is given, an error is set in system error.
+        Get configuration of the indexed sequencer. The configuration consists of multiple parameters in a JSON format.
+        If an invalid sequencer index is given, an error is set in system error.
 
         Parameters
         ----------
@@ -424,7 +517,8 @@ class TurboCluster(Cluster):
 
     def _set_pre_distortion_config(self, slot: int, pre_distortion_config: Any) -> None:
         """
-        Set pre-distortion configuration. The configuration consists of multiple parameters in a JSON format. If the configation does not have the correct format, an error is set in system error..
+        Set pre-distortion configuration. The configuration consists of multiple parameters in a JSON format.
+        If the configation does not have the correct format, an error is set in system error..
 
         Parameters
         ----------
@@ -451,7 +545,8 @@ class TurboCluster(Cluster):
 
     def _get_pre_distortion_config(self, slot: int) -> Any:
         """
-        Get pre-distortion configuration. The configuration consists of multiple parameters in a JSON format. If the configation does not have the correct format, an error is set in system error..
+        Get pre-distortion configuration. The configuration consists of multiple parameters in a JSON format.
+        If the configation does not have the correct format, an error is set in system error..
 
         Parameters
         ----------
@@ -486,6 +581,14 @@ class TurboCluster(Cluster):
 
 
 def readline(conn) -> str:
+    """Safe implementation of readline on socket without buffering.
+
+    The trivial solution `socket.makefile().readline()` is wrong, because
+    it uses a buffer. If there is more data available on the socket, then
+    some if it will be loaded into the buffer and discarded after the statement.
+    Dataloss is likely with `makefile` combined with binary reads on the socket.
+    """
+
     socket = conn._transport._socket
     buffer = io.BytesIO()
     b = None
@@ -495,6 +598,7 @@ def readline(conn) -> str:
         if b == b'\n':
             break
     return buffer.getvalue().decode().rstrip()
+
 
 if qblox_version >= Version('0.12'):
     def _convert_sequencer_status(state_str: str):
