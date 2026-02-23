e3cca5f0d09e43470ef3d4a98569f5cde17fc74e
Sander de Snoo <s.l.desnoo@tudelft.nl>
2026-02-23 10:51:08 +0100

    Moved q1asm_dump

--- q1pulse/__init__.py
+++ q1pulse/__init__.py
@@ -2,4 +2,4 @@ __version__ = "1.0.4"
 
 from .instrument import Q1Instrument, set_exception_on_overload
 from .lang.math_expressions import Lsr as lsr
-from .dumper import q1asm_dump
+from .util.dumper import q1asm_dump
